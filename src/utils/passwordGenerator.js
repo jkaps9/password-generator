b4a7226c86c1bfd3fda317c10902ad8533476e95
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-11 07:51:36 -0400

    feat: refactor password generation to use a centralized character set and improve array handling

--- src/utils/passwordGenerator.js
+++ src/utils/passwordGenerator.js
@@ -5,19 +5,54 @@ export function generateSecurePassword(
   includeNumbers,
   includeSymbols,
 ) {
-  let chars = "";
-  if (includeUppercase) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  if (includeLowercase) chars += "abcdefghijklmnopqrstuvwxyz";
-  if (includeNumbers) chars += "0123456789";
-  if (includeSymbols) chars += "!@#$%^&*()_+";
+  const sets = {
+    upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lower: "abcdefghijklmnopqrstuvwxyz",
+    numbers: "0123456789",
+    symbols: "!@#$%^&*()_+",
+  };
 
-  let generated = "";
+  let chars = "";
+  if (includeUppercase) chars += sets.upper;
+  if (includeLowercase) chars += sets.lower;
+  if (includeNumbers) chars += sets.numbers;
+  if (includeSymbols) chars += sets.symbols;
+
   const randomValues = new Uint32Array(length);
   window.crypto.getRandomValues(randomValues);
+  let tempIndex = 0;
 
-  for (let i = 0; i < length; i++) {
-    generated += chars[randomValues[i] % chars.length];
+  let passwordArray = [];
+
+  if (includeUppercase)
+    passwordArray.push(
+      sets.upper[randomValues[tempIndex++] % sets.upper.length],
+    );
+  if (includeLowercase)
+    passwordArray.push(
+      sets.lower[randomValues[tempIndex++] % sets.lower.length],
+    );
+  if (includeNumbers)
+    passwordArray.push(
+      sets.numbers[randomValues[tempIndex++] % sets.numbers.length],
+    );
+  if (includeSymbols)
+    passwordArray.push(
+      sets.symbols[randomValues[tempIndex++] % sets.symbols.length],
+    );
+
+  while (tempIndex < length) {
+    passwordArray.push(chars[randomValues[tempIndex] % chars.length]);
+    tempIndex++;
   }
 
-  return generated;
+  const shuffleValues = new Uint32Array(length);
+  window.crypto.getRandomValues(shuffleValues);
+
+  for (let i = passwordArray.length - 1; i > 0; i--) {
+    const j = shuffleValues[i] % (i + 1);
+    [passwordArray[i], passwordArray[j]] = [passwordArray[j], passwordArray[i]];
+  }
+
+  return passwordArray.join("");
 }
