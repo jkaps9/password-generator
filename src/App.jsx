5d23e41c3f694404aad07a6ce3c126fcde7147c9
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-10 23:30:41 -0400

    feat: implement password generator form with character length input

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,44 @@
+import { useState } from "react";
+
 export default function App() {
-  return <></>;
+  const [formData, setFormData] = useState({
+    length: 8,
+    includeUppercase: false,
+    includeLowerCase: false,
+    includeNumbers: false,
+    includeSymbols: false,
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <>
+      <header>
+        <h1>Password Generator</h1>
+      </header>
+      <main>
+        <form noValidate>
+          <div className="input-group">
+            <div>
+              <label htmlFor="range">Character Length</label>
+              <p>{formData.length}</p>
+            </div>
+            <input
+              type="range"
+              id="length"
+              name="length"
+              min="0"
+              max="20"
+              value={formData.length}
+              step="1"
+              onChange={handleInputChange}
+            />
+          </div>
+        </form>
+      </main>
+    </>
+  );
 }
