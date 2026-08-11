ed0dc0c89bee0ec3777d3002fa8a432a8e3a0284
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-11 00:17:09 -0400

    feat: enhance UI layout and improve form structure for password generation

--- src/App.jsx
+++ src/App.jsx
@@ -50,85 +50,90 @@ export default function App() {
         <h1>Password Generator</h1>
       </header>
       <main>
-        <div className="password-output">
+        <div className="card row">
           <p className={password === "" ? "faded" : undefined}>
             {password === "" ? "P4$5W0rD!" : password}
           </p>
           <button
+            className="btn"
             onClick={() => navigator.clipboard.writeText(password)}
             aria-label="copy password to clipboard"
           >
             <img src={CopyIcon} alt="" aria-hidden="true" />
           </button>
         </div>
-        <form onSubmit={handleSubmit} noValidate>
-          <div className="input-group">
-            <div>
-              <label htmlFor="range">Character Length</label>
-              <p>{formData.length}</p>
-            </div>
-            <input
-              type="range"
-              id="length"
-              name="length"
-              min="0"
-              max="20"
-              value={formData.length}
-              step="1"
-              onChange={handleInputChange}
-            />
-          </div>
-
-          <div className="input-group">
-            <div>
-              <input
-                type="checkbox"
-                id="includeUppercase"
-                name="includeUppercase"
-                onChange={handleInputChange}
-                checked={formData.includeUppercase}
-              />
-              <label htmlFor="includeUppercase">
-                Include Uppercase Letters
-              </label>
-            </div>
-            <div>
-              <input
-                type="checkbox"
-                id="includeLowercase"
-                name="includeLowercase"
-                onChange={handleInputChange}
-                checked={formData.includeLowercase}
-              />
-              <label htmlFor="includeLowercase">
-                Include Lowercase Letters
-              </label>
-            </div>
-            <div>
-              <input
-                type="checkbox"
-                id="includeNumbers"
-                name="includeNumbers"
-                onChange={handleInputChange}
-                checked={formData.includeNumbers}
-              />
-              <label htmlFor="includeNumbers">Include Numbers</label>
-            </div>
-            <div>
-              <input
-                type="checkbox"
-                id="includeSymbols"
-                name="includeSymbols"
-                onChange={handleInputChange}
-                checked={formData.includeSymbols}
-              />
-              <label htmlFor="includeSymbols">Include Symbols</label>
-            </div>
+        <div className="card">
+          <form onSubmit={handleSubmit} noValidate>
             <div className="input-group">
-              <button type="submit">Generate</button>
+              <div>
+                <label htmlFor="range">Character Length</label>
+                <p>{formData.length}</p>
+              </div>
+              <input
+                type="range"
+                id="length"
+                name="length"
+                min="0"
+                max="20"
+                value={formData.length}
+                step="1"
+                onChange={handleInputChange}
+              />
             </div>
-          </div>
-        </form>
+
+            <div className="input-group">
+              <div>
+                <input
+                  type="checkbox"
+                  id="includeUppercase"
+                  name="includeUppercase"
+                  onChange={handleInputChange}
+                  checked={formData.includeUppercase}
+                />
+                <label htmlFor="includeUppercase">
+                  Include Uppercase Letters
+                </label>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  id="includeLowercase"
+                  name="includeLowercase"
+                  onChange={handleInputChange}
+                  checked={formData.includeLowercase}
+                />
+                <label htmlFor="includeLowercase">
+                  Include Lowercase Letters
+                </label>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  id="includeNumbers"
+                  name="includeNumbers"
+                  onChange={handleInputChange}
+                  checked={formData.includeNumbers}
+                />
+                <label htmlFor="includeNumbers">Include Numbers</label>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  id="includeSymbols"
+                  name="includeSymbols"
+                  onChange={handleInputChange}
+                  checked={formData.includeSymbols}
+                />
+                <label htmlFor="includeSymbols">Include Symbols</label>
+              </div>
+              <div className="input-group">
+                <button className="btn btn--primary" type="submit">
+                  Generate
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </main>
     </>
   );
